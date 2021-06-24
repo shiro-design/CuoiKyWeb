e16dccca18615e9f1a031c41b76bce6df320c724
Nguyen Thanh Binh <ntb1080@gmail.com>
2021-06-24 11:56:52 +0700

    Edit

---
+++ Source + Bao Cao + Vid DEMO/404.php
@@ -0,0 +1,14 @@
+ <!-- purple x moss 2020 -->
+
+<head>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/404error.css">
+  <script src="https://kit.fontawesome.com/4b9ba14b0f.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <div class="mainbox">
+    <div class="err">4</div>
+    <i class="far fa-question-circle fa-spin"></i>
+    <div class="err2">4</div>
+    <div class="msg">Không may trang web này hiện không tồn tại<p>Hãy quay về  <a href="index.php">Trang Chủ nào</a></p></div>
+      </div>
